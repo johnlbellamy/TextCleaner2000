042d94a67d839e42beb128433b8fd86b02184129
John bellamy <johnb340@yahoo.com>
2018-11-17 14:51:49 -0700

    Updated codebase. Bug fixes

--- TextCleaner2000/TextCleaner.py
+++ TextCleaner2000/TextCleaner.py
@@ -4,58 +4,83 @@ import pickle
 import os
 
 class TextCleaner:
-    """Takes a string from array-like objects and performs:
-    alpha_iterator(text) -- Text in list (or array-like) format. Returns lower-case letters stripped of punctuation and numbers.
-    stop_word_iterator(text) --  Removes common "stop" words, like "and."
-    word_iterator(text, stop_words) -- Text and custom stop_words in list format. stop_words are words to be removed.
-    can use this in-lieu of stop_word_iterator, or in addition to.
+    """
+	Takes a string from array-like objects and performs:
+	Where text is in list (or array-like) format.
+        alpha_iterator(text) --  Returns lower-case letters stripped of punctuation and numbers.
+        stop_word_iterator(text) --  Removes common "stop" words, like "and."
+        custom_stop_word_iterator(text, stop_words) -- Custom stop_words in list format. stop_words are words to be removed.
+        can use this in-lieu of stop_word_iterator, or in addition to.
 	
 	GENERAL USAGE:
-	1) Download file to chosen directory.
-	2) from  TextCleaner2000.TextCleaner import TextCleaner
-	3) Pass the install directory to tell TextCleaner where to locate files and initialize a cleaner instance:
-	   WINDOWS: cleaner = TextCleaner("PATH\\TO\\INSTALL\\DIRECTORY\\TextCleaner2000")
-	   LINUX/UNIX/IOS: cleaner = TextCleaner("PATH/TO/INSTALL/DIRECTORY/TextCleaner2000")
-	4) Methods within a TextCleaner instance are instance methods so to use the functions call: cleaner.function_name(args)
+	    1) If use is for a simple project, with a couple python files, download file 
+        to root directory of project, where .py or .ipynb will live.
+    
+	Import:
+	    2) from  TextCleaner2000.TextCleaner import TextCleaner
+	
+	Instance Instantiation:
+        3a) For simple projects as described in 1), simply  instantiate a cleaner opbject with 
+        empty call: cleaner = TextCleaner()
+    
+	    3b) For more complicated projects pass the install directory to tell TextCleaner where to locate files and initialize a cleaner instance:
+	    WINDOWS: cleaner = TextCleaner("PATH\\TO\\INSTALL\\DIRECTORY\\TextCleaner2000")
+	    LINUX/UNIX/IOS: cleaner = TextCleaner("PATH/TO/INSTALL/DIRECTORY/TextCleaner2000")
+	
+	Call static methods:
+	    4) cleaner is static method so to use the functions call: cleaner.function_name
 	
 	EXAMPLE USAGE:
-	For the following examples, text refers to an array-like object. For best results, pass text as a list() or a Pandas DataFrame column: (assuming data_frame is a pandas DataFrame) data_frame["column_name"].
-    For custom stopword removal, pass as a list().	
+	    For the following examples, text refers to an array-like object. For best results, pass text as a list() or a Pandas DataFrame column: (assuming data_frame is a pandas DataFrame) data_frame["column_name"].
+        For custom stopword removal, pass as a list().	
 	
 	GENERAL NUMBER AND PUNCTUATION REMOVAL:
-	alpha_words = cleaner.alpha_iterator(text)
+	    alpha_words = cleaner.alpha_iterator(text)
 	
 	COMMON STOPWORD REMOVAL:
-	cleaned_of_stops = cleaner.stop_word_iterator(text)
+	    cleaned_of_stops = cleaner.stop_word_iterator(text)
 	
 	CUSTOM STOPWORD REMOVAL:
-	cleaned_of_custom_stops = cleaner.custom_stop_word_iterator(text, stop_words)
-	Remember that stop_words is a comma-separated list()."""
+	    cleaned_of_custom_stops = cleaner.custom_stop_word_iterator(text, stop_words)
+	    Remember that stop_words is a comma-separated list()."""
     
-    def __init__(self, tc_2000_home):
+    def __init__(self, tc_2000_home = ""):
         self.tc_2000_home = tc_2000_home
-        pickle_file_path = os.path.join(tc_2000_home, 'stops.pkl')
-        pkl_file = open(pickle_file_path, 'rb')
-        self.stop_words = pickle.load(pkl_file) 
-        pkl_file.close() 
-              
+        pickle_file_path = 'stops.pkl'
+        
+        if tc_2000_home == "":
+            pkl_file = open(os.path.join(os.getcwd() + '\\TextCleaner2000',pickle_file_path), 'rb')
+            self.stop_words = pickle.load(pkl_file) 
+            pkl_file.close() 
+        else:
+            pkl_file = open(os.path.join(tc_2000_home,pickle_file_path), 'rb')
+            self.stop_words = pickle.load(pkl_file) 
+            pkl_file.close() 
 
-    def __alphaizer(self, text):
-        """Given a string (text), removes all punctuation and numbers.
+    def __alphaizer(self, text, remove_numeric = True):
+        self.text = text
+        """
+		 Given a string (text), removes all punctuation and numbers.
         Returns lower-case words. Called by the iterator method
         alpha_iterator to apply this to lists, or array-like (pandas dataframe)
-        objects."""
-        self.text = text
-      
-        x = re.sub("[0-9,'--!@#$%=&:;<>\?\(\)\.\{\}\]\[\\\/-]+", " " , text)
-        x = x.lower() #Makes letters lower-case
-        x = re.sub("^\s" , "" , x) #Removes leading spaces
-        x = re.sub("\s+$" ,"" , x) # Removes trailing spaces
-        x = re.sub("  "," ", x) #Removes extra spaces
-        
-        return x
+        objects. """
+        if remove_numeric:
+            result = ''.join(i for i in text if not i.isdigit())
+            x = re.sub(r",'--!@#$%=&:;<>?().{}][/-]+\"", " " , result)
+            x = re.sub(r"^\s" , "" , x) #Removes leading spaces
+            x = re.sub(r"\s+$" ,"" , x) # Removes trailing spaces
+            x = re.sub(r"  "," ", x) #Removes extra spaces
+            x = x.replace('\\','').lower()
+            return x
+        else:
+            x = re.sub(r",'--!@#$%=&:;<>?().{}][/-]+\"", " " , text)
+            x = re.sub(r"^\s" , "" , x) #Removes leading spaces
+            x = re.sub(r"\s+$" ,"" , x) # Removes trailing spaces
+            x = re.sub(r"  "," ", x) #Removes extra spaces
+            x = x.replace('\\','').lower() #Removes back-space
+            return x
+	       
                 
-
     def __stop_word_remover(self, text, stop):
         """Removes common stop-words like: "and", "or","but", etc. Called by
         stop_word_iterator to apply this to lists, or array-like (pandas dataframe)
@@ -81,7 +106,7 @@ class TextCleaner:
         
         return text2
   
-    def alpha_iterator(self,text):
+    def alpha_iterator(self,text, remove_numeric = True):
         """Calls __alphaizer to apply this method to array-like objects. Usage:
         TextCleaner.alphaizer(text)."""
 
