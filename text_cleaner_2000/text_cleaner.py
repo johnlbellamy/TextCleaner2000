1094e4c6e8aa1c1bcfcd659fd4f7d5bf8c2b680b
John bellamy <johnb340@yahoo.com>
2018-11-27 13:28:53 -0700

    Changed module/ file names

--- text_cleaner_2000/text_cleaner.py
+++ text_cleaner_2000/text_cleaner.py
@@ -52,7 +52,7 @@ class TextCleaner:
         pickle_file_path = 'stops.pkl'
         
         if tc_2000_home == "":
-            pkl_file = open(os.path.join(os.getcwd() + '\\TextCleaner2000',pickle_file_path), 'rb')
+            pkl_file = open(os.path.join(os.getcwd() + '\\text_cleaner_2000',pickle_file_path), 'rb')
             self.stop_words = pickle.load(pkl_file) 
             pkl_file.close() 
         else:
